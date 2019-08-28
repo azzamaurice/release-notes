b183b8c3b293e9e6e8ad5062ffb117d2000eaff0
azzamaurice <aaron.maurice@gmail.com>
2019-08-28 17:34:15 +1000

    Finished Journal render component

--- components/Journal.vue
+++ components/Journal.vue
@@ -1,4 +1,6 @@
 <script>
+import { map } from 'lodash-es'
+
 export default {
     name: 'Journal',
     props: {
@@ -11,10 +13,53 @@ export default {
         fixes: [Array, String]
     },
     render(h) {
-        const general = h(`p`, this.general)
-        const journal = h(`p`, this.journal)
+        const output = []
 
-        return h(`div`, [general, journal])
+        if (this.general) {
+            output.push(h(`p`, this.general))
+        }
+
+        if (this.journal) {
+            output.push(h(`p`, this.journal))
+        }
+
+        if (this.behind) {
+            output.push(h(`h2`, `Behind the Scenes`))
+            output.push(h(`p`, `Behind the scenes, we've been working on the following areas:`))
+            output.push(h(`ul`, map(this.behind, i => h(`li`, i))))
+        }
+
+        if (this.beta) {
+            output.push(h(`h2`, `Beta Improvements`))
+
+            if (this.beta && this.behind) {
+                output.push(h(`p`, `We've also made a number of improvements to our existing BETA features, including:`))
+            } else {
+                output.push(h(`p`, `To start off with, we've made the following improvements to our existing BETA features:`))
+            }
+
+            output.push(h(`ul`, map(this.beta, i => h(`li`, i))))
+        }
+
+        if (this.features) {
+            output.push(h(`h2`, `New Features`))
+            output.push(h(`p`, `We're pleased to announce the following new features have been released this week!`))
+            output.push(h(`ul`, map(this.features, i => h(`li`, i))))
+        }
+
+        if (this.improvements) {
+            output.push(h(`h2`, `General Improvements`))
+            output.push(h(`p`, `We've made some general improvements to the system this week, including:`))
+            output.push(h(`ul`, map(this.improvements, i => h(`li`, i))))
+        }
+
+        if (this.fixes) {
+            output.push(h(`h2`, `Bug Fixes`))
+            output.push(h(`p`, `In addition to the above news, we also fixed the following bugs:`))
+            output.push(h(`ul`, map(this.fixes, i => h(`li`, i))))
+        }
+
+        return h(`div`, output)
     }
 }
 </script>
